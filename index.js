bb8f9305fef8f4070ad682b16e56a12feb1cd550
Abdurahman Sherif <abdurahman.sherif@gmail.com>
2018-05-20 16:05:12 -0400

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+/* Donation bot description */
+
+// npm packages
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/', function(req, res){
+  res.send('Hello World!!')
+})
+
+app.get('/', function(req, res){
+  if(req.query['hub.verify_token'] ===
+    'my_voice_is_my_password_verify_me') {
+      res.send(req.query['hub.challenge'])
+  }
+  res.send('Failed to verify token!')
+})
+
+app.listen(app.get('port'), function(){
+  console.log('Running on port', app.get('port'))
+})
