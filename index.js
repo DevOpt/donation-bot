fe37d36921d5712127166659981a683c1bfe74e9
Abdurahman Sherif <abdurahman.sherif@gmail.com>
2018-05-20 16:20:45 -0400

    init

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', function(req, res){
   res.send('Hello World!!')
 })
 
-app.get('/webook/', function(req, res){
+app.get('/webhook/', function(req, res){
   if(req.query['hub.verify_token'] ===
     'my_voice_is_my_password_verify_me') {
       res.send(req.query['hub.challenge'])
